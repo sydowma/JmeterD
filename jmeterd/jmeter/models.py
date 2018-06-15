a9189cc720186c4538fd8610c90b18852e80b445
magaofei <magaofei@100tal.com>
2018-06-15 11:34:09 +0800

    完善 model

--- jmeterd/jmeter/models.py
+++ jmeterd/jmeter/models.py
@@ -10,23 +10,55 @@ __doc__ = """
 class Machine(models.Model):
     """
     """
-    name = models.CharField()
+    name = models.CharField(
+        "机器名",
+        max_length=20,
+        null=False,
+        blank=False,
+        default=""
+    )
     port = models.PositiveIntegerField(
+        '机器端口',
         blank=False,
         null=False,
         default=22
     )
-    intranet_ip = models.IPAddressField()
-    external_ip = models.IPAddressField()
-    password = models.CharField()
-    secret_key = models.FileField()
-    status = models.PositiveSmallIntegerField()
+    ip = models.IPAddressField(
+        'IP地址',
+        blank=False,
+        null=False,
+        default="127.0.0.1"
+    )
+    password = models.CharField(
+        'password',
+        max_length=50,
+        blank=False,
+        null=False,
+        default=""
+
+    )
+    secret_key = models.FileField(
+        '秘钥文件',
+        null=False,
+        blank=True
+    )
+    status = models.PositiveSmallIntegerField(
+        '状态, 离线/在线',
+        blank=False,
+        null=False,
+        default=0
+
+    )
     gmt_create = models.DateTimeField(
         "创建时间",
-        "",
+        null=False,
+        auto_now_add=True,
+    )
+    gmt_modified = models.DateTimeField(
+        '修改时间',
+        null=False,
         auto_now=True
     )
-    gmt_modified = models.DateTimeField()
 
     class Meta:
         db_table = 'jmeter_machine'
@@ -36,36 +68,98 @@ class Machine(models.Model):
 class File(models.Model):
     """
     """
-    file = models.FileField()
+    name = models.CharField(
+        "文件名",
+        max_length=20,
+        null=False,
+        blank=False,
+        default=""
+    )
+    
+    # 状态，0 未知，  1不存在， 2存在
+    status = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+        default=0
+    )
+    file = models.FileField(
+        '文件',
+        null=False,
+        blank=False,
+    )
 
 
 class AbstractTask(models.Model):
 
     jmx_file = models.ForeignKey(
         File,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        default=0
     )
     data_file = models.ForeignKey(
         File,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=False,
+        blank=True
     )
 
-    name = models.CharField()
-    run_time = models.DateTimeField()
-    loops = models.SmallIntegerField()
-    num_threads = models.PositiveIntegerField()
+    name = models.CharField(
+        '任务名',
+        blank=False,
+        null=False,
+        default=""
+    )
+    run_time = models.DateTimeField(
+        '执行时间',
+        null=False,
+        blank=False
+    )
+    loops = models.SmallIntegerField(
+        '循环次数',
+        null=False,
+        blank=False,
+        default=1
+    )
+    num_threads = models.PositiveIntegerField(
+        '线程数',
+        null=False,
+        blank=False,
+        default=1
+    )
+
+    scheduler = models.BooleanField(
+        '调度器',
+        null=False,
+        blank=False,
+        default=False
+    )
+
+    duration = models.PositiveIntegerField(
+        '持续时间',
+        null=False,
+        blank=False,
+        default=0
+    )
 
 
     source_machine = models.ForeignKey(
         Machine,
         on_delete=models.CASCADE,
-        db_constraint=False
+        db_constraint=False,
+        null=False,
+        blank=False,
+        verbose_name='压力源机器'
     )
-    target_machine = models.ForeignKey(
-        Machine,
-        on_delete=models.CASCADE,
-        db_constraint=False
-    )
+    # target_machine = models.ForeignKey(
+    #     Machine,
+    #     on_delete=models.CASCADE,
+    #     db_constraint=False,
+    #     null=False,
+    #     blank=False,
+    #     verbose_name='目标机器'
+    # )
 
     class Meta:
         abstract = True
@@ -74,13 +168,34 @@ class AbstractTask(models.Model):
 class Task(AbstractTask):
     """
     """
-    status = models.PositiveSmallIntegerField()
+    status = models.BooleanField(
+        '任务状态',
+        null=False,
+        blank=True,
+        default=True,
+    )
+    
+    task_start_time = models.DateTimeField(
+        '任务结束时间',
+        null=False,
+        blank=False
+    )
+    task_end_time = models.DateTimeField(
+        '任务结束时间',
+        null=False,
+        blank=False
+    )
+
     gmt_create = models.DateTimeField(
         "创建时间",
-        "",
-        auto_now=True
+        null=False,
+        auto_now_add=True,
     )
-    gmt_modified = models.DateTimeField()
+    gmt_modified = models.DateTimeField(
+        '修改时间',
+        null=False,
+        auto_now=True,
+    )
     
     class Meta:
         db_table = 'jmeter_task'
@@ -105,15 +220,30 @@ class TaskResult(AbstractTask):
 
 
 class Host(models.Model):
-    ip = models.IPAddressField()
-    server = models.URLField()
+    ip = models.IPAddressField(
+        'IP地址',
+        null=False,
+        blank=False,
+        default='127.0.0.1'
+        
+    )
+    server = models.URLField(
+        '域名',
+        null=False,
+        blank=False,
+        default=""
+    )
 
     gmt_create = models.DateTimeField(
         "创建时间",
-        "",
-        auto_now=True
+        null=False,
+        auto_now_add=True
     )
-    gmt_modified = models.DateTimeField()
+    gmt_modified = models.DateTimeField(
+        '修改时间',
+        null=False,
+        auto_now=True,
+    )
 
     class Meta:
         db_table = 'jmeter_host'
@@ -124,20 +254,41 @@ class Config(models.Model):
     """
     配置
     """
-    jmeter_report_path = models.FilePathField()
-    jmeter_path = models.FilePathField()
-    jtl_path = models.FilePathField()
+    jmeter_report_path = models.FilePathField(
+        '报告存放路径',
+        null=False,
+        blank=False,
+        default=""
+    )
+    jmeter_path = models.FilePathField(
+        'JMeter存放路径',
+        null=False,
+        blank=False,
+        default=""
+    )
+    jtl_path = models.FilePathField(
+        'Jtl文件存放路径',
+        null=False,
+        blank=False,
+        default=""
+    )
     host = models.ForeignKey(
         Host,
         on_delete=models.CASCADE,
-        db_constraint=False
+        db_constraint=False,
+        null=False,
+        blank=False
     )
     gmt_create = models.DateTimeField(
         "创建时间",
-        "",
+        null=False,
+        auto_now_add=True
+    )
+    gmt_modified = models.DateTimeField(
+        '修改时间',
+        null=False,
         auto_now=True
     )
-    gmt_modified = models.DateTimeField()
 
     class Meta:
         db_table = 'jmeter_config'
