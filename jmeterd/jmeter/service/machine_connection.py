304caf044f5c1cb03d1b7d7f76beff60ff18f530
Mark <mamian521@gmail.com>
2018-06-15 23:11:13 +0800

    完善 machine_connection

--- jmeterd/jmeter/service/machine_connection.py
+++ jmeterd/jmeter/service/machine_connection.py
@@ -2,7 +2,51 @@ from fabric import Connection
 from jmeter.models import Machine
 import sys
 
-class LocalMachine():
+
+class MachineConfig():
+
+    def __init__(self):
+        
+        self.host = None
+        self.user = None
+        self.port = None
+        self.password = None
+        
+
+class MachineConnection():
+
+    def __init__(self, machine):
+        """
+        """
+        self.machine = machine
+        self.connection = None
+        self._connect()
+
+
+    def _machines(self):
+        """
+        机器数
+        拿出Model中的数据
+        """
+    
+    def _connect(self):
+        """
+        连接
+        """
+
+        """  如果机器数量为空，就使用本地机器  """
+        if self.machine is None:
+            self.connection = Connection('local')
+        else:
+            self.connection = Connection(
+                host=self.machine.host,
+                user=self.machine.user,
+                port=self.machine.port,
+                connect_kwargs={
+                    'password': self.machine.password
+                }
+            )
+
     
     @property
     def platform(self):
@@ -12,7 +56,7 @@ class LocalMachine():
         return sys.platform
 
     def is_linux(self):
-        return sys.platform == 'linux2'
+        return 'linux' in sys.platform or 'Linux' in sys.platform
     
     def is_mac(self):
         return sys.platform == 'darwin'
@@ -20,24 +64,6 @@ class LocalMachine():
     def is_windows(self):
         return sys.platform == 'win32'
         
-
-
-class MachineConnection():
-
-    def __init__(self, machine):
-        """
-        """
-        self.machine = machine
-        self._connect()
-    
-    def _connect(self):
-        """
-        连接
-        """
-
-        """  如果机器数量为空，就使用本地机器  """
-        if self.machine is None:
-            pass
     
     @property
     def status(self):
